0cb0e5f5f45c057d953d6d0024493105c6622a9f
BottomHalf <istiyaq.mi9@gmail.com>
2024-12-12 14:44:15 +0530

    imporved

--- ApiGateway.Core/Configuration/Startup.cs
+++ ApiGateway.Core/Configuration/Startup.cs
@@ -84,7 +84,7 @@ namespace ApiGateway.Core.Configuration
             services.AddSingleton<IFetchGithubConfigurationService>(x =>
                 FetchGithubConfigurationService
                 .getInstance()
-                .Init(ApplicationNames.EMSTUM)
+                .Init(ApplicationNames.EMSTUM, _environment)
             );
         }
 
@@ -101,7 +101,14 @@ namespace ApiGateway.Core.Configuration
             ProducerConfig producerConfig = new ProducerConfig();
             _configuration.Bind("KafkaServerDetail", producerConfig);
             services.AddSingleton(producerConfig);
-            services.AddSingleton((Func<IServiceProvider, IKafkaProducerService>)((IServiceProvider x) => KafkaProducerService.SubscribeKafkaService(ApplicationNames.EMSTUM, x.GetRequiredService<ProducerConfig>())));
+
+            services.AddSingleton<IKafkaProducerService>(x => 
+                KafkaProducerService.GetInstance(
+                    ApplicationNames.EMSTUM, 
+                    x.GetRequiredService<ProducerConfig>(),
+                    _builder.Environment
+                )
+            );
         }
 
         private void ConfiguraAppSettingFiles(ConfigurationManager _configuration, IWebHostEnvironment _environment)
